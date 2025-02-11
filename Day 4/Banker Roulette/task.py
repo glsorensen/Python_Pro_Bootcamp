f368c18eff4fa45d34d2d11b92ffd18a38f34c35
glsorensen <gunnar.l.sorensen@gmail.com>
2025-02-11 15:11:21 -0700

    Choose random items from a list

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,12 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_friend = random.choice(friends)
+print(random_friend)
+
+
+random_friend_index = random.randint(0, len(friends) -1)
+print(random_friend_index)
+random_friend_item = friends[random_friend_index]
+print(random_friend_item)
