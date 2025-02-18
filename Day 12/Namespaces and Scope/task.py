6a138bebc45aa1cac6ab66f6b43d09734825ed53
glsorensen <gunnar.l.sorensen@gmail.com>
2025-02-18 14:29:48 -0700

    Global Vriable

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,12 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+
+def a_function(a_parameter):
+    a_variable = 15
+    return a_parameter
+
+
+a_function(10)
+print(a_variable)
